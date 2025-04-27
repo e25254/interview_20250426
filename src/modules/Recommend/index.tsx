35a7edcd9bb8d32cf146aaae7681b0f95f29ee0c
JerryJie <e25254@gmail.com>
2025-04-27 18:56:05 +0800

    feat( style ) : modify style ( #main )

--- src/modules/Recommend/index.tsx
+++ src/modules/Recommend/index.tsx
@@ -5,7 +5,9 @@ import React from "react";
 export default function Recommend() {
   return (
     <Stack className="shrink-0 py-3 gap-4 overflow-hidden pr-0">
-      <h3 className="text-xl font-bold text-font px-hor-container">推介</h3>
+      <h3 className="text-xl font-bold text-black-500 px-hor-container">
+        推介
+      </h3>
       <Stack
         className={`h-full flex-row gap-4 overflow-x-auto px-hor-container scrollbarHide`}
       >
