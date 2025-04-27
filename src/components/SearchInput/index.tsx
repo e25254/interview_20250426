f98ad16de60f17ca245526e654114c0a79ae720d
JerryJie <e25254@gmail.com>
2025-04-27 16:38:49 +0800

    feat( SearchInput ) : `SearchInput` ui completed ( #main )

--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -1,11 +1,36 @@
+"use client";
 import React from "react";
 import Stack from "../Stack";
-
+import Search from "../icon/SearchIcon";
+import useSearchInput from "./useSearchInput";
 export default function SearchInput() {
+  const {
+    isShowPlaceholder,
+    showPlaceholder,
+    hidePlaceholder,
+    inputValue,
+    handleInputChange,
+  } = useSearchInput();
   return (
-    <Stack className="w-full justify-center items-center py-2">
-      {/* <input type="text" /> */}
-      １２３
+    <Stack className="w-full justify-center items-center py-2 px-hor-container">
+      <input
+        type="text"
+        className="w-full bg-gray-300 rounded-sm text-xs py-2 px-2 focus-visible:outline-none caret-gray-500"
+        onFocus={hidePlaceholder}
+        onBlur={showPlaceholder}
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      {isShowPlaceholder && (
+        <Stack className="gap-1.5 absolute pointer-events-none text-sm flex-row">
+          <Stack className="h-5 aspect-square">
+            <Search fill="var(--color-gray-500)" />
+          </Stack>
+          <span className="whitespace-nowrap text-gray-500 font-bold">
+            搜尋
+          </span>
+        </Stack>
+      )}
     </Stack>
   );
 }
--- src/components/SearchInput/useSearchInput.ts
+++ src/components/SearchInput/useSearchInput.ts
@@ -1,3 +1,34 @@
+import { useCallback, useMemo, useState } from "react";
+
 export default function useSearchInput() {
-  return {};
+  const [inputValue, setInputValue] = useState("");
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleInputChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setInputValue(e.target.value);
+    },
+    []
+  );
+
+  const showPlaceholder = useCallback(() => {
+    setIsFocused(false);
+  }, []);
+
+  const hidePlaceholder = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const shouldShowPlaceholder = useMemo(
+    () => !isFocused && inputValue === "",
+    [isFocused, inputValue]
+  );
+
+  return {
+    isShowPlaceholder: shouldShowPlaceholder,
+    showPlaceholder,
+    hidePlaceholder,
+    inputValue,
+    handleInputChange,
+  };
 }
