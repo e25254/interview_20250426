6b0ec7cf43fa453cf93df15591527de0c238dbc5
JerryJie <e25254@gmail.com>
2025-04-27 19:15:28 +0800

    feat( style ) : add ellipsis ( #main )

--- src/components/SmallApp/index.tsx
+++ src/components/SmallApp/index.tsx
@@ -5,9 +5,11 @@ export default function SmallApp() {
   return (
     <Stack className="gap-2.5">
       <Stack className="w-[86px] aspect-square bg-gray-500 rounded-3xl"></Stack>
-      <Stack className="gap-1.5">
-        <p className="text-xs font-bold text-black-300">Fyuse - 3D 相片</p>
-        <span className="text-xs text-gray-500 font-bold">
+      <Stack className="gap-2">
+        <p className="text-xs font-bold text-black-300 two-line-ellipsis">
+          Fyuse - 3D 相片
+        </p>
+        <span className="text-xs text-gray-500 font-bold two-line-ellipsis">
           照片和視訊照片和視訊
         </span>
       </Stack>
