8f1277d9edffe8ca7cfdf2a508afadbd3c109086
JerryJie <e25254@gmail.com>
2025-05-02 01:48:25 +0800

    feat( AppSearch ) : modify debounce delay time && add useCallback in handleSearch ( #main )

--- src/containers/AppSearch/useAppSearch.ts
+++ src/containers/AppSearch/useAppSearch.ts
@@ -1,27 +1,29 @@
 import { useAppDispatch, useAppSelector } from "@/store";
 import { setSearchTerm } from "@/store/slices/searchSlice";
 import { debounce } from "lodash";
-import { useEffect, useState, useMemo } from "react";
+import { useEffect, useState, useMemo, useCallback } from "react";
 
 export default function useAppSearch() {
   const dispatch = useAppDispatch();
   const searchTerm = useAppSelector((state) => state.search);
   const [localSearchTerm, setLocalSearchTerm] = useState(searchTerm);
 
-  // 創建 debounced dispatch 函數
   const debouncedDispatch = useMemo(
     () =>
       debounce((value: string) => {
         dispatch(setSearchTerm(value));
-      }, 1000),
+      }, 300),
     [dispatch]
   );
 
-  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    setLocalSearchTerm(value);
-    debouncedDispatch(value);
-  };
+  const handleSearch = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const value = e.target.value;
+      setLocalSearchTerm(value);
+      debouncedDispatch(value);
+    },
+    [debouncedDispatch]
+  );
 
   useEffect(() => {
     return () => {
