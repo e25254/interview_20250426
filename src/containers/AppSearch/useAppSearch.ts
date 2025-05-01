2362d72f29bf478c2071892bf590a30c4ce0270f
JerryJie <e25254@gmail.com>
2025-05-02 01:46:40 +0800

    feat( AppSearch ) : add debounce when input search ( #main )

--- src/containers/AppSearch/useAppSearch.ts
+++ src/containers/AppSearch/useAppSearch.ts
@@ -1,13 +1,36 @@
 import { useAppDispatch, useAppSelector } from "@/store";
 import { setSearchTerm } from "@/store/slices/searchSlice";
+import { debounce } from "lodash";
+import { useEffect, useState, useMemo } from "react";
+
 export default function useAppSearch() {
   const dispatch = useAppDispatch();
   const searchTerm = useAppSelector((state) => state.search);
+  const [localSearchTerm, setLocalSearchTerm] = useState(searchTerm);
+
+  // 創建 debounced dispatch 函數
+  const debouncedDispatch = useMemo(
+    () =>
+      debounce((value: string) => {
+        dispatch(setSearchTerm(value));
+      }, 1000),
+    [dispatch]
+  );
+
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    dispatch(setSearchTerm(e.target.value));
+    const value = e.target.value;
+    setLocalSearchTerm(value);
+    debouncedDispatch(value);
   };
+
+  useEffect(() => {
+    return () => {
+      debouncedDispatch.cancel();
+    };
+  }, [debouncedDispatch]);
+
   return {
-    searchTerm,
+    searchTerm: localSearchTerm,
     handleSearch,
   };
 }
